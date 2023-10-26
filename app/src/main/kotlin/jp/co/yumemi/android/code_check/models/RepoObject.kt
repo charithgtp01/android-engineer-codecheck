236e3fbcc0194e0bd15ba6dcc79ed68b528b5bee
charithvithanage <charithvin@gmail.com>
2023-10-26 23:30:38 +0530

    Create Owner and RepoObject Data classes

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/models/RepoObject.kt
@@ -0,0 +1,29 @@
+package jp.co.yumemi.android.code_check.models
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+/**
+ * Data class for Git Hub Object
+ */
+@Parcelize
+data class RepoObject(
+    val name: String?,
+    val owner: Owner?,
+    @SerializedName("language")
+    private val nullableLanguage: String?,
+    @SerializedName("stargazers_count")
+    val stargazersCount: Long?,
+    @SerializedName("watchers_count")
+    val watchersCount: Long?,
+    @SerializedName("forks_count")
+    val forksCount: Long?,
+    @SerializedName("open_issues_count")
+    val openIssuesCount: Long?,
+) : Parcelable {
+    //Set Default Value to language variable
+    val language: String
+        get() = nullableLanguage ?: "No Language Data"
+}
+
