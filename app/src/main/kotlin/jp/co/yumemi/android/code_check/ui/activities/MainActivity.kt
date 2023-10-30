5b53cf4d915242b94c805b68d756641416e74847
charithgtp01 <charithvin@gmail.com>
2023-10-30 14:25:04 +0530

    Add bottom menu to Main Activity Add Bottom menu icons

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/activities/MainActivity.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/activities/MainActivity.kt
@@ -3,17 +3,9 @@
  */
 package jp.co.yumemi.android.code_check.ui.activities
 
-import android.os.Bundle
-import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 import jp.co.yumemi.android.code_check.R
-import jp.co.yumemi.android.code_check.constants.DialogConstants
-import jp.co.yumemi.android.code_check.constants.MessageConstants.EXIT_CONFIRMATION_MESSAGE
-import jp.co.yumemi.android.code_check.interfaces.ConfirmDialogButtonClickListener
-import jp.co.yumemi.android.code_check.interfaces.CustomAlertDialogListener
-import jp.co.yumemi.android.code_check.utils.DialogUtils
-import java.util.*
 
 /**
  * Main Activity Page
