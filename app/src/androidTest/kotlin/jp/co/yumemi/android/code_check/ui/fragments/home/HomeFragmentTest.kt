95431ece840e03ab1fcde0e79481d10a5e8c5f89
charithgtp01 <charithvin@gmail.com>
2023-11-12 01:36:14 +0530

    Wrote UI test for Github Details page navigation from HomeFragment and Main Activity

--- app/src/androidTest/kotlin/jp/co/yumemi/android/code_check/ui/fragments/home/HomeFragmentTest.kt
+++ app/src/androidTest/kotlin/jp/co/yumemi/android/code_check/ui/fragments/home/HomeFragmentTest.kt
@@ -1,11 +1,12 @@
 package jp.co.yumemi.android.code_check.ui.fragments.home
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.pressImeActionButton
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
@@ -45,8 +46,6 @@ class HomeFragmentTest {
 
         // Check if the RecyclerView is displayed
         onView(withId(R.id.recyclerView)).check(matches(isDisplayed()))
-
-        // Add more assertions based on your UI
     }
 
     @Test
@@ -74,6 +73,29 @@ class HomeFragmentTest {
 
         // Check if an error message for no internet is displayed
         onView(withText(R.string.no_internet)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testNavigateToRepositoryFragment() {
+        // Type a valid search query in the search view
+        onView(withId(R.id.searchInputText)).perform(typeText("Android"))
+
+        // Click on the search button
+        onView(withId(R.id.searchInputText)).perform(pressImeActionButton())
+
+        Thread.sleep(5000)
+
+        onView(withId(R.id.recyclerView)).check(matches(isDisplayed())).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RepoListAdapter.RepoListViewHolder>(
+                0,
+                click()
+            )
+        )
+        // Verify that the RepositoryFragment is launched
+        onView(withId(R.id.mainLayout)).check(matches(isDisplayed()))
+
+        // Verify that the correct data is passed to the RepositoryFragment
+        onView(withId(R.id.nameView)).check(matches(withText("Android")))
     }
 
     @After
